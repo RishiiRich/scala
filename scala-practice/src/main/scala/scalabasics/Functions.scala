706e1c96907d096585cab4c0c158126752f8388b
Rishi Arora <arora.rishi01@gmail.com>
2022-11-16 23:56:46 +0530

    added recursive functions

--- scala-practice/src/main/scala/scalabasics/Functions.scala
+++ scala-practice/src/main/scala/scalabasics/Functions.scala
@@ -13,4 +13,11 @@ object Functions extends App {
   println(aParameterlessFunction())
   //println(aParameterlessFunction) // only works in scala 2
 
+  def aRepeatedFunction(aString: String, n : Int) : String = {
+    if (n == 1) aString
+    else aString + aRepeatedFunction(aString, n-1)
+  }
+
+  println(aRepeatedFunction("Hello", 4))
+
 }
